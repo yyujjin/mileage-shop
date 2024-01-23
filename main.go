6b59b15b618a49a473bcd481f849e7b9a0885c31
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-01-24 00:32:07 +0900

    유저 생성자 함수 구현,잔여 마일리지 확인기능 구현

--- main.go
+++ main.go
@@ -2,7 +2,35 @@ package main
 
 import "fmt"
 
+type user struct {
+	name  string
+	point int
+	cart  string
+}
+
+// 1. 포인터 함수가 되려면 내보내는 데이터타입 앞에 * 붙이고
+func newUser(name string) user {
+	d := user{
+		name, 1000000, "",
+	}
+	return d
+}
+
 func main() {
+	user1 := newUser("박유진")
+	user2 := newUser("박수현")
+	user3 := newUser("박정숙")
+	//2. 여기에도 구조체 이름 앞에 * 붙이기 => why? 포인터 구조체들이 모여있는 배열이라서 (데이터 타입이 달라서 일반 구조체와 포이터 구조체를 같은 배열안에 담을 수 없음 )
+	users := []user{}
+	users = append(
+		users,
+		user1,
+		user2,
+		user3,
+	)
+	for i := 0; i < len(users); i++ {
+		fmt.Printf("%v : %v\n", users[i].name, users[i].point)
+	}
 
 	type item struct {
 		name   string
@@ -26,13 +54,19 @@ func main() {
 		fmt.Scan(&menu)
 		switch menu {
 		case 1:
-			fmt.Println("구매 화면입니다.")
+			fmt.Println("구매")
 		case 2:
-			fmt.Println("장바구니 화면입니다.")
+			fmt.Println("잔여 수량 확인")
 		case 3:
-			fmt.Println("프로그램이 종료됩니다.")
+			fmt.Println("잔여 마일리지 확인")
+			fmt.Printf("%v님의 잔여 마일리지는 %v점 입니다.", users[0].name, users[0].point)
+		case 4:
+			fmt.Println("배송 상태 확인")
+		case 5:
+			fmt.Println("장바구니 확인")
+		case 6:
+			fmt.Println("프로그램 종료")
 			return
-
 		}
 	}
 
