ee70c564178de710b92ed56fc7543d1822e904b4
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-01-24 21:48:35 +0900

    구매 시 잘못된 입력 및 잔여 수량 제한 로직 추가

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type user struct {
 	name  string
@@ -8,7 +10,6 @@ type user struct {
 	cart  string
 }
 
-// 1. 포인터 함수가 되려면 내보내는 데이터타입 앞에 * 붙이고
 func newUser(name string) user {
 	d := user{
 		name, 1000000, "",
@@ -20,7 +21,7 @@ func main() {
 	user1 := newUser("박유진")
 	user2 := newUser("박수현")
 	user3 := newUser("박정숙")
-	//2. 여기에도 구조체 이름 앞에 * 붙이기 => why? 포인터 구조체들이 모여있는 배열이라서 (데이터 타입이 달라서 일반 구조체와 포이터 구조체를 같은 배열안에 담을 수 없음 )
+
 	users := []user{}
 	users = append(
 		users,
@@ -28,35 +29,84 @@ func main() {
 		user2,
 		user3,
 	)
+
 	for i := 0; i < len(users); i++ {
 		fmt.Printf("%v : %v\n", users[i].name, users[i].point)
 	}
 
 	type item struct {
+		id     int
 		name   string
 		point  int
 		amount int
 	}
-
-	items := []item{
-		{"텀블러", 10000, 30},
-		{"롱패딩", 500000, 20},
-		{"투미 백팩", 400000, 20},
-		{"나이키 운동화", 150000, 50},
-		{"빼빼로", 1200, 500},
+	//Go 언어에서 슬라이스는 참조 타입입니다. 여기에만 * 붙여주면 된대. 쓰려는 곳에 & 이거 안붙여줘도 된다는데 뮤슨말?
+	//그럼 밑에 value에서는 새 값이 나타난거잖아 for range 안에서?
+	//*이걸 적어줌으로써 value도 items의 값을 참조하게 되고 그 참조한 값으로 값을 변경해줘서 바뀌는건가?
+	items := []*item{
+		{1, "텀블러", 10000, 30},
+		{2, "롱패딩", 500000, 20},
+		{3, "투미 백팩", 400000, 20},
+		{4, "나이키 운동화", 150000, 50},
+		{5, "빼빼로", 1200, 500},
 	}
-
-	fmt.Println(items)
 
 	var menu int
 
 	for {
+		fmt.Println(
+			"메뉴의 번호를 입력하세요.\n" +
+				"1.구매\n" +
+				"2.잔여 수량 확인\n" +
+				"3.잔여 마일리지 확인\n" +
+				"4.배송 상태 확인\n" +
+				"5.장바구니 확인\n" +
+				"6.프로그램 종료")
 		fmt.Scan(&menu)
 		switch menu {
 		case 1:
 			fmt.Println("구매")
+			for _, value := range items {
+				fmt.Printf("물품%v : %v, 가격: %v원, 잔여 수량: %v\n", value.id, value.name, value.point, value.amount)
+			}
+			var num int
+
+			fmt.Print("구매 할 물품 번호를 입력하세요: ")
+			fmt.Scan(&num)
+			if num < 1 || num > 5 {
+				for {
+					fmt.Println("잘못된 입력입니다. 1~5까지의 숫자를 입력해주세요.(메뉴로 돌아가려면 0번을 누르세요)")
+					fmt.Scan(&num)
+					if num >= 1 && num <= 5 {
+						break
+					}
+					if num == 0 {
+						break
+					}
+				}
+				if num == 0 {
+					continue
+				}
+			}
+			fmt.Printf("구매할 상품 : %v\n", num)
+			for _, value := range items {
+				if num == value.id {
+					if value.amount > 0 {
+						users[0].point -= value.point
+						value.amount--
+						//items[index].amount-- 포인터 안만들었을 때
+						fmt.Println("구매가 완료되었습니다.")
+					} else {
+						fmt.Println("잔여 수량이 부족하여 구매가 불가능합니다.")
+					}
+
+				}
+			}
 		case 2:
 			fmt.Println("잔여 수량 확인")
+			for _, value := range items {
+				fmt.Printf("%v의 잔여 수량은 %v입니다.", value.name, value.amount)
+			}
 		case 3:
 			fmt.Println("잔여 마일리지 확인")
 			fmt.Printf("%v님의 잔여 마일리지는 %v점 입니다.", users[0].name, users[0].point)
@@ -67,9 +117,8 @@ func main() {
 		case 6:
 			fmt.Println("프로그램 종료")
 			return
+		default:
+			fmt.Println("잘못된 입력입니다. 1~6까지의 숫자를 입력해 주세요.")
 		}
 	}
-
-	// 사용자가 3을 입력하기 전까진 계속 메뉴를 입력받을 수 있음
-	// 실행 -> 1 -> 구매 화면~ -> 엔터 -> 메뉴를 다시 입력 가능 -> 2 -> 장바구니~~ -> 엔터 -> 메뉴 입력 가능 -> 3 -> 프로그램이 종료~
 }
