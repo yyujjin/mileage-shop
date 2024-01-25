4240d5dbf85305e762443c0b192391698d3e3598
yyujjin <141253669+yyujjin@users.noreply.github.com>
2024-01-25 22:06:28 +0900

    구매 코드 패키지로 이동

--- main.go
+++ main.go
@@ -5,17 +5,11 @@ import (
 	"mileage-shop/src/menu"
 )
 
-func newUser(name string) user {
-	d := user{
+func newUser(name string) menu.User {
+	d := menu.User{
 		name, 1000000, "",
 	}
 	return d
-}
-
-type user struct {
-	name  string
-	point uint
-	cart  string
 }
 
 func main() {
@@ -24,7 +18,7 @@ func main() {
 	var user2 = newUser("박수현")
 	var user3 = newUser("박정숙")
 
-	var users = []user{}
+	var users = []menu.User{}
 	users = append(
 		users,
 		user1,
@@ -33,7 +27,7 @@ func main() {
 	)
 
 	for i := 0; i < len(users); i++ {
-		fmt.Printf("%v : %v\n", users[i].name, users[i].point)
+		fmt.Printf("%v : %v\n", users[i].Name, users[i].Point)
 	}
 
 	var inputMenu int
@@ -47,51 +41,16 @@ func main() {
 				"4.배송 상태 확인\n" +
 				"5.장바구니 확인\n" +
 				"6.프로그램 종료")
+
 		fmt.Scan(&inputMenu)
+
 		switch inputMenu {
 		case 1:
-			fmt.Println("구매")
-			for _, value := range menu.Items {
-				fmt.Printf("물품%v : %v, 가격: %v원, 잔여 수량: %v\n", value.Id, value.Name, value.Point, value.Amount)
-			}
-			var num int
-
-			fmt.Print("구매 할 물품 번호를 입력하세요: ")
-			fmt.Scan(&num)
-			if num < 1 || num > 5 {
-				for {
-					fmt.Println("잘못된 입력입니다. 1~5까지의 숫자를 입력해주세요.(메뉴로 돌아가려면 0번을 누르세요)")
-					fmt.Scan(&num)
-					if num >= 1 && num <= 5 {
-						break
-					}
-					if num == 0 {
-						break
-					}
-				}
-				if num == 0 {
-					continue
-				}
-			}
-			fmt.Printf("구매할 상품 : %v\n", num)
-			for _, value := range menu.Items {
-				if num == value.Id {
-					if value.Amount > 0 && users[0].point >= value.Point {
-						users[0].point -= value.Point
-						value.Amount--
-						//items[index].amount-- 포인터 안만들었을 때
-						fmt.Println("구매가 완료되었습니다.")
-					} else if value.Amount < 0 {
-						fmt.Println("잔여 수량이 부족하여 구매가 불가능합니다.")
-					} else {
-						fmt.Println("보유 포인트가 부족하여 구매가 불가능합니다.")
-					}
-				}
-			}
+			menu.Purchase(users...)
 		case 2:
 			menu.RemainingAmount()
 		case 3:
-			menu.RemainingMileage(users[0].name, users[0].point)
+			menu.RemainingMileage(users[0].Name, users[0].Point)
 		case 4:
 			menu.DeliveryStatus()
 		case 5:
--- src/menu/menu.go
+++ src/menu/menu.go
@@ -19,6 +19,62 @@ var Items = []*Item{
 	{5, "빼빼로", 1200, 500},
 }
 
+type User struct {
+	Name  string
+	Point uint
+	Cart  string
+}
+
+func Purchase(users ...User) {
+	fmt.Println("구매")
+	for _, value := range Items {
+		fmt.Printf("물품%v : %v, 가격: %v원, 잔여 수량: %v\n", value.Id, value.Name, value.Point, value.Amount)
+	}
+	var num int
+
+	fmt.Print("구매 할 물품 번호를 입력하세요: ")
+	fmt.Scan(&num)
+	if num < 1 || num > 5 {
+		for {
+			fmt.Println("잘못된 입력입니다. 1~5까지의 숫자를 입력해주세요.(메뉴로 돌아가려면 0번을 누르세요)")
+			fmt.Scan(&num)
+			if num >= 1 && num <= 5 {
+				break
+			}
+			if num == 0 {
+				return
+			}
+		}
+	}
+	fmt.Printf("구매할 상품 : %v\n", num)
+	for _, value := range Items {
+		if num == value.Id {
+			if value.Amount > 0 && users[0].Point >= value.Point {
+				users[0].Point -= value.Point
+				value.Amount--
+				//items[index].amount-- 포인터 안만들었을 때
+				fmt.Println("구매가 완료되었습니다.")
+			} else if value.Amount < 0 {
+				fmt.Println("잔여 수량이 부족하여 구매가 불가능합니다.")
+			} else {
+				fmt.Println("보유 포인트가 부족하여 구매가 불가능합니다.")
+			}
+		}
+	}
+}
+
+func RemainingAmount() {
+	fmt.Println("잔여 수량 확인")
+	for _, value := range Items {
+		fmt.Printf("%v의 잔여 수량은 %v입니다.\n", value.Name, value.Amount)
+	}
+}
+
+func RemainingMileage(name string, point uint) {
+	fmt.Println("잔여 마일리지 확인")
+	fmt.Printf("%v님의 잔여 마일리지는 %v점 입니다.", name, point)
+}
+
 func DeliveryStatus() {
 	fmt.Println("배송 상태 확인")
 }
@@ -29,18 +85,6 @@ func CheckCart() {
 
 func ExitProgram() {
 	fmt.Println("프로그램 종료")
-}
-
-func RemainingMileage(name string, point uint) {
-	fmt.Println("잔여 마일리지 확인")
-	fmt.Printf("%v님의 잔여 마일리지는 %v점 입니다.", name, point)
-}
-
-func RemainingAmount() {
-	fmt.Println("잔여 수량 확인")
-	for _, value := range Items {
-		fmt.Printf("%v의 잔여 수량은 %v입니다.\n", value.Name, value.Amount)
-	}
 }
 
 //패키지를 사용해서 따로 빼두는 이유는 관련있는 함수나 변수끼리 모아두기 위해서
